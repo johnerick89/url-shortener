81199f03cf00545a06b2ab767dfa0da6f0d7a488
john.mboga <john.mboga@jumo.world>
2020-08-28 00:07:13 +0300

    Created notes.txt
    Signed-off-by: john.mboga <john.mboga@jumo.world>

---
+++ url_shortener_frontend/src/containers/UrlsListContainer.js
@@ -0,0 +1,44 @@
+import React from "react";
+import UrlItem from "../components/UrlItem";
+
+import { makeStyles } from "@material-ui/core/styles";
+import { List, Divider, Card } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+function UrlsListContainer(props) {
+  const classes = useStyles();
+
+  function renderUrls() {
+    let uiItems = [];
+    props.urls.forEach((url, index) => {
+      if (index) {
+        uiItems.push(<Divider component="li" />);
+      }
+      uiItems.push(
+        <UrlItem
+          key={url.id}
+          card={url}
+        />
+      );
+    });
+
+    return uiItems;
+  }
+
+  return (
+    <Card
+      style={{ width: "800px", minWidth: "800px", marginBottom: "20px" }}
+      variant="outlined"
+    >
+      <List className={classes.root}>{renderUrls()}</List>
+    </Card>
+  );
+}
+
+export default UrlsListContainer;
