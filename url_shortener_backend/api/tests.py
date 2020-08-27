700a7e86a04c62107e44bdd7db2c5989ac974b24
john.mboga <john.mboga@jumo.world>
2020-08-28 00:00:17 +0300

    Modified tests
    Signed-off-by: john.mboga <john.mboga@jumo.world>

--- url_shortener_backend/api/tests.py
+++ url_shortener_backend/api/tests.py
@@ -3,6 +3,7 @@ from rest_framework import status
 from django.test import TestCase, Client
 from django.urls import resolve, reverse
 import json
+from rest_framework.test import APIClient
 from .serializers import UrlSerializer
 from .models import Url
 
@@ -30,6 +31,9 @@ class UrlTest(TestCase):
 
 class GetAllUrlsTest(TestCase):
     """ Test module for GET all Urls API """
+    def _api_client(self):
+        client = APIClient()
+        return client
 
     def setUp(self):
         Url.objects.create(
@@ -38,16 +42,19 @@ class GetAllUrlsTest(TestCase):
             original='http://www.facebook.com', tiny='http://localhost:8000/')
 
     def test_get_all_urls(self):
-        response = self.client.get(resolve('/urls/'))
+        client = self._api_client()
+        response = client.get(f"/api/urls/")
 
         urls = Url.objects.all()
         serializer = UrlSerializer(urls, many=True)
-        self.assertEqual(response, serializer.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
 
 class CreateNewUrlTest(TestCase):
     """ Test module for inserting a new url """
+    def _api_client(self):
+        client = APIClient()
+        return client
 
     def setUp(self):
         self.valid_payload = {
@@ -58,19 +65,24 @@ class CreateNewUrlTest(TestCase):
         }
 
     def test_create_valid_url(self):
-        path = resolve('/urls/')
-        print(path)
+        client = self._api_client()
         response = client.post(
-            path,
+            f"/api/urls/",
             data=json.dumps(self.valid_payload),
-            content_type='application/json'
+            content_type="application/json",
         )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_create_invalid_url(self):
+        client = self._api_client()
         response = client.post(
             resolve('/urls/'),
             data=json.dumps(self.invalid_payload),
             content_type='application/json'
+        )
+        response = client.post(
+            f"/api/urls/",
+            data=json.dumps(self.invalid_payload),
+            content_type="application/json",
         )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
